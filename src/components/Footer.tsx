d1292123f06eda41d96d38132de7498b671fae38
Aline Prasser <aline_bravin@hotmail.com>
2025-06-10 00:32:24 -0300

    chore: changing footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex items-center bg-background h-10 justify-center">
+    <footer className="w-full flex items-center px-10 py-4 text-xs sm:text-base h-12 bg-background sm:h-10 justify-center">
       <p className="text-sm text-muted-foreground">
-        &copy; 2025 Matheus Melotti. Todos os direitos reservados.{" "}
-        <a
-          href="https://www.linkedin.com/in/aline-bravin-prasser/"
-          target="_blank"
-          className="hover:underline transition-all duration-300">
-          Feito com ❤️
-        </a>
+        &copy; 2025 Matheus Melotti. Todos os direitos reservados. Feito com ❤️
       </p>
     </footer>
   );
